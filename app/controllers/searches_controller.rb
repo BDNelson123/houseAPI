472f0ab10ad8df2a522ca1632746023321018caf
BDNelson123 <ben.david.nelson@gmail.com>
2014-07-03 15:33:02 +0000

    only need primary image

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    render json: Home.joins("LEFT JOIN images ON images.home_id = homes.id").select('homes.*,array_agg(images.image) AS images').where('address ilike ? OR address2 ilike ?', "%#{params[:query]}%", "%#{params[:query]}%").group("homes.id")
+    render json: Home.joins("LEFT JOIN images ON images.home_id = homes.id AND images.primary = true").select('homes.*,array_agg(images.image) AS images').where('address ilike ? OR address2 ilike ? OR city ilike ? OR state ilike ? OR cast(zip as text) ilike ?', "%#{params[:query]}%", "%#{params[:query]}%", "%#{params[:query]}%", "%#{params[:query]}%", "%#{params[:query]}%").group("homes.id")
   end
 end
