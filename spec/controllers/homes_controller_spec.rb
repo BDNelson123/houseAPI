d22cb104b79568e572b88b302e15375cfe83b085
BDNelson123 <ben.david.nelson@gmail.com>
2014-09-23 21:04:37 +0000

    fixed tests for homes controller

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,48 +1,39 @@
 require 'spec_helper'
 include ActionDispatch::TestProcess
 
-describe HomesController do
-  before(:each) do
-    User.delete_all
-    Home.delete_all
-    Zillow.delete_all
-    Image.delete_all
-
-    user1 = FactoryGirl.create(:user, id: 1, auth_token: "D5zXdpU8a_7gmojN3k1bOg")
-    user2 = FactoryGirl.create(:user, id: 2, auth_token: "D5zXdpU8a_7gmojN3k1333")
-
-    home1 = FactoryGirl.create(:home, id: 3, user_id: 1)
-    home2 = FactoryGirl.create(:home, id: 4, user_id: 2)
-
-    image1 = FactoryGirl.create(:image, id: 4, user_id: 1, home_id: 3)
-    image2 = FactoryGirl.create(:image, id: 5, user_id: 2, home_id: 4)
-
-    zillow1 = FactoryGirl.create(:zillow, id: 6, user_id: 1, home_id: 3)
-    zillow2 = FactoryGirl.create(:zillow, id: 7, user_id: 2, home_id: 4)
+describe HomesController, :type => :api do
+  before(:all) do
+    @user = FactoryGirl.create(:user)
+    @home = FactoryGirl.create(:home, user_id: @user.id)
+    @image = FactoryGirl.create(:image, user_id: @user.id, home_id: @home.id)
+    @zillow = FactoryGirl.create(:zillow, user_id: @user.id, home_id: @home.id)
   end
 
-  describe "index" do
-    it "should return 1 total row" do
-      response = Home.home_joins.home_attributes.where(:user_id => 1, :active => true).group("homes.id, zillows.id")
-      expect(response.length).to eq(1)
-    end
+  describe "#index" do
+    context "basic is false" do
+      it "should return 1 total row" do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(@user.auth_token)
+        get :index, { 'user_id' => @user.id }
+        expect(JSON.parse(response.body).length).to eq(1)
+      end
 
-    it "should return the id of the home" do
-      response = Home.home_joins.home_attributes.where(:user_id => 1, :active => true).group("homes.id, zillows.id").to_json
-      parsed_json = ActiveSupport::JSON.decode(response)
-      expect(parsed_json[0]['id']).to eq(3)
-    end
+      it "should return all zillow attributes such as yearBuilt (not basic)" do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(@user.auth_token)
+        get :index, { 'user_id' => @user.id, 'basic' => 'false' }
+        JSON.parse(response.body)[0].should include('yearBuilt')
+      end
 
-    it "should return the id of the image" do
-      response = Home.home_joins.home_attributes.where(:user_id => 1, :active => true).group("homes.id, zillows.id").to_json
-      parsed_json = ActiveSupport::JSON.decode(response)
-      expect(parsed_json[0]['images'][0]['id']).to eq(4)
-    end
+      it "should return the correct value for yearBuilt if params[:basic] == false" do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(@user.auth_token)
+        get :index, { 'user_id' => @user.id, 'basic' => 'false' }
+        expect(JSON.parse(response.body)[0]['yearBuilt'].to_i).to eq(@zillow.yearBuilt.to_i)
+      end
 
-    it "should return the url of the image" do
-      response = Home.home_joins.home_attributes.where(:user_id => 1, :active => true).group("homes.id, zillows.id").to_json
-      parsed_json = ActiveSupport::JSON.decode(response)
-      expect(parsed_json[0]['images'][0]['image']['url']).to eq('/uploads/image/image/4/test.jpg')
+      it "should return a response of 200" do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(@user.auth_token)
+        get :index, { 'user_id' => @user.id, 'basic' => 'false' }
+        expect(response.status).to eq(200)
+      end
     end
   end
 end
