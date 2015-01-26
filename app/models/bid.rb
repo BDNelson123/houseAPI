3f4dbbbe072abeac4b8dc3be4d552d4cc845f890
BDNelson123 <ben.david.nelson@gmail.com>
2015-01-26 23:26:05 +0000

    one more validation for price (integer) and validation specs for price

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -6,7 +6,7 @@ class Bid < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :home_id, :presence => true
   validates :price, :presence => true
-  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :only_integer => true}
 
   scope :bid_show_attributes, -> { select('
     bids.id as bids_id,
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe Bid do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    context "presence = true" do
+      it "should return one validation error for price" do
+        bid = Bid.new(:price => nil, :user_id => 1, :home_id => 1)
+        bid.should have(3).error_on(:price)
+        bid.should have(0).error_on(:user_id)
+        bid.should have(0).error_on(:home_id)
+      end
+
+      it "should return one validation error for user_id" do
+        bid = Bid.new(:price => 1000, :user_id => nil, :home_id => 1)
+        bid.should have(0).error_on(:price)
+        bid.should have(1).error_on(:user_id)
+        bid.should have(0).error_on(:home_id)
+      end
+
+      it "should return one validation error for home_id" do
+        bid = Bid.new(:price => 1000, :user_id => 1, :home_id => nil)
+        bid.should have(0).error_on(:price)
+        bid.should have(0).error_on(:user_id)
+        bid.should have(1).error_on(:home_id)
+      end
+    end
+
+    context "price validations" do
+      # Price is invalid
+      # Price must be greater than 0
+      it "should have two validation errors if the integer is negative" do
+        bid = Bid.new(:price => -100, :user_id => 1, :home_id => 1)
+        bid.should have(2).error_on(:price)
+      end
+
+      # Price is not a number
+      it "should have one validation errors if the integer is a string" do
+        bid = Bid.new(:price => "test", :user_id => 1, :home_id => 1)
+        bid.should have(1).error_on(:price)
+      end
+    end
+  end
 end
