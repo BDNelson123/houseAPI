4f11a5874a3e0611f1d8e86529972ddb4eb61c3e
BDNelson123 <ben.david.nelson@gmail.com>
2015-01-26 21:23:11 +0000

    model validations for images

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
+include ActionDispatch::TestProcess
 
 describe Image do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    context "presence = true" do
+      it "should return one validation error for user_id" do
+        image = Image.new(:image => fixture_file_upload('photos/test.jpg', 'image/jpg'), :user_id => nil, :home_id => 1, :klass => "home")
+        image.should have(1).error_on(:user_id)
+        image.should have(0).error_on(:image)
+        image.should have(0).error_on(:klass)
+      end
+
+      it "should return one validation error for image" do
+        image = Image.new(:image => nil, :user_id => 1, :home_id => 1, :klass => "home")
+        image.should have(0).error_on(:user_id)
+        image.should have(1).error_on(:image)
+        image.should have(0).error_on(:klass)
+      end
+
+      it "should return one validation error for klass" do
+        image = Image.new(:image => fixture_file_upload('photos/test.jpg', 'image/jpg'), :user_id => 1, :home_id => 1, :klass => nil)
+        image.should have(0).error_on(:user_id)
+        image.should have(0).error_on(:image)
+        image.should have(1).error_on(:klass)
+      end
+    end
+  end
 end
