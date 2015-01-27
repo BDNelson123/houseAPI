a1c040bac3eafb3518cf546bb3ef3fa6921f8365
BDNelson123 <ben.david.nelson@gmail.com>
2015-01-27 17:27:53 +0000

    message validation specs

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,14 +5,14 @@ class Message
   field :message, :type => String
   field :sender_id, :type => Integer
   field :receiver_id, :type => Integer
-  field :home_id, :type => Integer
   field :thread_id, :type => Integer
+  field :home_id, :type => Integer
 
   validates :sender_id, presence: true
   validates :receiver_id, presence: true
-  validates :home_id, presence: true
   validates :message, presence: true
   validates :thread_id, presence: true
+  validates :home_id, presence: true
 
   def self.thread_id(sender_id,receiver_id)
     thread = Message.any_of({:sender_id => sender_id, :receiver_id => receiver_id}, {:sender_id => receiver_id, :receiver_id => sender_id}).limit(1).first
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,61 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    context "presence = true" do
+      it "should return one validation error for sender_id" do
+        message = Message.new(:sender_id => nil, :receiver_id => 2, :message => "test message", :thread_id => 1, :home_id => 1)
+        message.should have(1).error_on(:sender_id)
+        message.should have(0).error_on(:receiver_id)
+        message.should have(0).error_on(:message)
+        message.should have(0).error_on(:thread_id)
+        message.should have(0).error_on(:home_id)
+      end
+
+      it "should return one validation error for receiver_id" do
+        message = Message.new(:sender_id => 1, :receiver_id => nil, :message => "test message", :thread_id => 1, :home_id => 1)
+        message.should have(0).error_on(:sender_id)
+        message.should have(1).error_on(:receiver_id)
+        message.should have(0).error_on(:message)
+        message.should have(0).error_on(:thread_id)
+        message.should have(0).error_on(:home_id)
+      end
+
+      it "should return one validation error for message" do
+        message = Message.new(:sender_id => 1, :receiver_id => 1, :message => nil, :thread_id => 1, :home_id => 1)
+        message.should have(0).error_on(:sender_id)
+        message.should have(0).error_on(:receiver_id)
+        message.should have(1).error_on(:message)
+        message.should have(0).error_on(:thread_id)
+        message.should have(0).error_on(:home_id)
+      end
+
+      it "should return one validation error for thread_id" do
+        message = Message.new(:sender_id => 1, :receiver_id => 1, :message => "test message", :thread_id => nil, :home_id => 1)
+        message.should have(0).error_on(:sender_id)
+        message.should have(0).error_on(:receiver_id)
+        message.should have(0).error_on(:message)
+        message.should have(1).error_on(:thread_id)
+        message.should have(0).error_on(:home_id)
+      end
+
+      it "should return one validation error for thread_id" do
+        message = Message.new(:sender_id => 1, :receiver_id => 1, :message => "test message", :thread_id => 1, :home_id => nil)
+        message.should have(0).error_on(:sender_id)
+        message.should have(0).error_on(:receiver_id)
+        message.should have(0).error_on(:message)
+        message.should have(0).error_on(:thread_id)
+        message.should have(1).error_on(:home_id)
+      end
+
+      it "should return five validation errors as all five attributes are nil" do
+        message = Message.new(:sender_id => nil, :receiver_id => nil, :message => nil, :thread_id => nil, :home_id => nil)
+        message.should have(1).error_on(:sender_id)
+        message.should have(1).error_on(:receiver_id)
+        message.should have(1).error_on(:message)
+        message.should have(1).error_on(:thread_id)
+        message.should have(1).error_on(:home_id)
+      end
+    end
+  end
 end
