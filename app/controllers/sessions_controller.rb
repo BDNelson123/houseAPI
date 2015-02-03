831f3465f7343b7ffa45995a4ddbc3d5a5fbcbfb
BDNelson123 <ben.david.nelson@gmail.com>
2015-02-03 20:14:55 +0000

    sessions controller specs and small fix to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < ApplicationController
-  respond_to :json
-
   def show
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
@@ -8,11 +6,5 @@ class SessionsController < ApplicationController
     else
       render json: "Your credentials were not found.  Please try again", :status => :unprocessable_entity
     end
-  end
-
-  def user_params
-    _params = params.require(:user).permit(
-      :email, :password, :password_confirmation
-    )
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe SessionsController do
+  before(:all) do
+    @user = FactoryGirl.create(:user, :password => "testing123", :password_confirmation => "testing123")
+  end
 
+  after(:all) do
+    User.destroy_all
+  end
+
+  describe "#show" do
+    context "correct login credentials" do
+      it "should return the users information on a correct user login" do
+        get :show, { :id => "show", :email => @user.email, :password => "testing123" }
+        expect(response.status).to eq(200)
+      end
+
+      it "should return one user record" do
+        get :show, { :id => "show", :email => @user.email, :password => "testing123" }
+        expect(JSON.parse(response.body).length).to eq(9) # 9 fields
+      end
+
+      it "should return one user record" do
+        get :show, { :id => "show", :email => @user.email, :password => "testing123" }
+        expect(JSON.parse(response.body)["id"]).to eq(@user.id)
+        expect(JSON.parse(response.body)["auth_token"]).to eq(@user.auth_token)
+        expect(JSON.parse(response.body)["email"]).to eq(@user.email)
+        expect(JSON.parse(response.body)["firstname"]).to eq(@user.firstname)
+        expect(JSON.parse(response.body)["lastname"]).to eq(@user.lastname)
+      end
+    end
+
+    context "incorrect login credentials" do
+      it "should return Your credentials were not found.  Please try again if the password is wrong" do
+        get :show, { :id => "show", :email => @user.email, :password => "wrongPassword123" }
+        expect(response.body).to eq("Your credentials were not found.  Please try again")
+      end
+
+      it "should return Your credentials were not found.  Please try again if the email is wrong" do
+        get :show, { :id => "show", :email => "wrong@email.com", :password => "testing123" }
+        expect(response.body).to eq("Your credentials were not found.  Please try again")
+      end
+    end
+  end
 end
